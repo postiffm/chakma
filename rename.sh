fd97a4c8a6efab05390a6fa0e14eda6e27af59c6
Matt Postiff <postiffm@umich.edu>
2023-02-06 07:49:37 -0500

    Rename files to match Paratext convention

---
+++ rename.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Rename Bibledit filenames to original SFM file naming convention
+git mv 40_Matthew.usfm          41MATCCPBIV.SFM
+git mv 41_Mark.usfm		42MRKCCPBIV.SFM
+git mv 42_Luke.usfm		43LUKCCPBIV.SFM
+git mv 43_John.usfm		44JHNCCPBIV.SFM
+git mv 44_Acts.usfm		45ACTCCPBIV.SFM
+git mv 45_Romans.usfm		46ROMCCPBIV.SFM
+git mv 46_1_Corinthians.usfm	471COCCPBIV.SFM
+git mv 47_2_Corinthians.usfm	482COCCPBIV.SFM
+git mv 48_Galatians.usfm	49GALCCPBIV.SFM
+git mv 49_Ephesians.usfm	50EPHCCPBIV.SFM
+git mv 50_Philippians.usfm	51PHPCCPBIV.SFM
+git mv 51_Colossians.usfm	52COLCCPBIV.SFM
+git mv 52_1_Thessalonians.usfm	531THCCPBIV.SFM
+git mv 53_2_Thessalonians.usfm	542THCCPBIV.SFM
+git mv 54_1_Timothy.usfm	551TICCPBIV.SFM
+git mv 55_2_Timothy.usfm	562TICCPBIV.SFM
+git mv 56_Titus.usfm		57TITCCPBIV.SFM
+git mv 57_Philemon.usfm		58PHMCCPBIV.SFM
+git mv 58_Hebrews.usfm		59HEBCCPBIV.SFM
+git mv 59_James.usfm		60JASCCPBIV.SFM
+git mv 60_1_Peter.usfm		611PECCPBIV.SFM
+git mv 61_2_Peter.usfm		622PECCPBIV.SFM
+git mv 62_1_John.usfm		631JNCCPBIV.SFM
+git mv 63_2_John.usfm		642JNCCPBIV.SFM
+git mv 64_3_John.usfm		653JNCCPBIV.SFM
+git mv 65_Jude.usfm		66JUDCCPBIV.SFM
+git mv 66_Revelation.usfm	67REVCCPBIV.SFM
+
